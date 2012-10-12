e903a9b0e692057701b79cfcb3bf077f2d245788
Robert Helmer <rhelmer@rhelmer.org>
2012-10-11 20:56:33 -0700

    no longer used

--- crashstats/crashstats/models.py
+++ crashstats/crashstats/models.py
@@ -259,24 +259,6 @@ class CrashesPerAdu(SocorroMiddleware):
         return self.fetch(url % params)
 
 
-class Crashes(SocorroMiddleware):
-
-    def get(self, product, versions, os_names, start_date, end_date):
-        params = {
-            'product': product,
-            'versions': '+'.join(versions),
-            'os_names': '+'.join(os_names),
-            'start_date': start_date.strftime('%Y-%m-%d'),
-            'end_date': end_date.strftime('%Y-%m-%d'),
-        }
-
-        self.urlencode_params(params)
-        url = ('/crashes/daily/product/%(product)s/versions/%(versions)s/'
-               'from_date/%(start_date)s/to_date/%(end_date)s/'
-               'date_range_type/report/os/%(os_names)s' % params)
-        return self.fetch(url)
-
-
 class TCBS(SocorroMiddleware):
 
     def get(self, product, version, crash_type, end_date, duration,
